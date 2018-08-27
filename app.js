7ccf013d27ee173e5c9f90348e980567f2882265
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-08-27 16:10:31 -0700

    Add url parameters

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.listen(3000, function () {
+  console.log('Gif Search listening on port 3000!');
+});
+
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
